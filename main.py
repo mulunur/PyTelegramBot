c7b3996dee17edde20eb1abe53ede33b7adbedc0
Taisia <tayisiak@gmail.com>
2021-06-18 11:22:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import telebot
-def print_hi(name):
 
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+TOKEN = "1814314219:AAGGUNpYh279YU_Z7mP380ff9y91NU7SpPM"
+bot = telebot.TeleBot(TOKEN)
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "how are you doing?")
 
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    if message.text == 'Привет':
+        bot.reply_to(message, "Привет создатель")
+    else:
+        bot.reply_to(message, message.text)
+
+bot.polling()
