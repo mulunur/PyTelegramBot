ff93b58236b13a19d06d28a48ac20aaab8135402
Taisia <tayisiak@gmail.com>
2021-06-22 15:43:45 +0300

    telegram bot

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ name = ""
 awake_hour = ""
 time_hours = ""
 
-
 @bot.message_handler(commands=['start'])
 def command_start(message):
     bot.send_chat_action(message.chat.id, 'typing')
@@ -29,17 +28,18 @@ def command_start(message):
 
 @bot.message_handler(content_types=['text'])
 def user_entering_name(message):
-    if type (message.text) is str:
-        global name
-        name = message.text
+    global name
+    try:
+        name = str(message.text)
         bot.send_message(message.chat.id, name + messages.ok_name)
         bot.register_next_step_handler(message, user_entering_time)
-    else:
-        bot.send_message(message.chat.id, messages.reset_hello)
-        return
+    except Exception:
+        bot.send_message(message.chat.id, messages.hello_error)
+        bot.register_next_step_handler(message, user_entering_name)
 
 
 def user_entering_time(message):
+    global awake_hour
     try:
         awake_hour = int(message.text)
         if awake_hour < 0 or awake_hour > 24:
@@ -52,9 +52,9 @@ def user_entering_time(message):
 
 
 def user_entering_hours(message):
+    global time_hours
     try:
         time_hours = int(message.text)
-        print(type (time_hours))
         if time_hours < 3 or time_hours > 12:
             raise ValueError
         bot.send_message(message.chat.id, messages.ok_hours)
@@ -68,13 +68,23 @@ def calculate_alarm_clock(message):
     global name
     global time_hours
     global awake_hour
+    try:
+        sleep_minutes = int(time_hours) * 60 + 16
+        number_of_cycles = int(sleep_minutes / 90)
+        sleep_time = int((awake_hour - (number_of_cycles * 90 / 60)) % 24)
+        sleep_time_hours = str(int(sleep_time))
+        sleep_time_minutes = int(60/sleep_time%1)
+        if sleep_time_minutes < 10:
+            sleep_time_minutes = '0' + str(sleep_time_minutes)
+        sleep_time_minutes = str(sleep_time_minutes)
+        sleep_time_string = sleep_time_hours + ':' + sleep_time_minutes
+        bot.send_message(message.chat.id, name + messages.sleep_time[0] + str(awake_hour) +
+                     messages.sleep_time[1] + str(time_hours) + messages.sleep_time[2] +
+                         sleep_time_string + messages.sleep_time[3])
+    except Exception:
+        bot.send_message(message.chat.id, messages.calculation_error + messages.ok_name)
+        bot.register_next_step_handler(message, user_entering_time)
 
-    sleep_minutes = int(time_hours) * 60
-    number_of_cycles = sleep_minutes / 90
-    sleep_time = awake_hour + (number_of_cycles * 90 / 60)
-    bot.send_message(message.chat.id, '''name + messages.sleep_time[0] + awake_hour +
-                     messages.sleep_time[1] + time_hours + messages.sleep_time[2] +
-                     sleep_time + messages.sleep_time[3]''')
 
 
 @bot.message_handler(commands=['help'])
